a8001f94c649addf33eee68c2d10a2cbe196f7a2
KavehK7 <kavehk22@gmail.com>
2017-12-15 12:39:56 -0700

    Revert "Add files via upload"
    This reverts commit e7c4b4d0712e914ebd34baeefda24befc589c528.

--- jschr.io-master/app/index.ts
+++
@@ -1,6 +0,0 @@
-import mount from './mount'
-import ssr from './ssr'
-
-if (typeof document !== 'undefined') mount()
-
-export default ssr
--- jschr.io-master/infrastructure/modules/app/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "domain"                   { }
-variable "app_package"              { }
-variable "render_interval"          { }
-variable "env_vars"                 { type = "map" }
-variable "mailgun_smtp_password"    { }
